fe2e5a8f3a9a4a0d602191ca13f917e88ca3764c
luis-fbs <luis-fbs@outlook.com>
2024-08-07 19:59:05 -0300

    Define ação do botão de cadastrar pessoa

---
+++ src/main/java/principal/actionListeners/BotaoCadastrarPessoaActionListener.java
@@ -0,0 +1,46 @@
+package principal.actionListeners;
+
+import principal.util.ReferenciaURL;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class BotaoCadastrarPessoaActionListener implements ActionListener {
+    JTextField cpf, senha, nome;
+    JTextArea curriculo;
+
+    public BotaoCadastrarPessoaActionListener(JTextField cpf, JTextArea curriculo, JTextField nome, JTextField senha) {
+        this.cpf = cpf;
+        this.curriculo = curriculo;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try{
+            String json = """
+                    {
+                    "cpf": "%s",
+                    "senha": "%s",
+                    "nome": "%s",
+                    "foto": "sf",
+                    "curriculo": "%s"
+                    }
+                    """.formatted(cpf.getText(), senha.getText(), nome.getText(), curriculo.getText());
+
+            String url = ReferenciaURL.getReferenciaURL() + "/usuario/cadastrar";
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder(new URI(url))
+                                             .method("POST", HttpRequest.BodyPublishers.ofString(json))
+                                             .build();
+            HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        }catch (Exception exception){}
+    }
+}
