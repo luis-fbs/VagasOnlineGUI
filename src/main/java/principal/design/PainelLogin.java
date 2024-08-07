0d37b1f2c7c7d406d17c2d2345e2fdef563de6c6
luis-fbs <luis-fbs@outlook.com>
2024-08-07 13:40:24 -0300

    Painel de login

---
+++ src/main/java/principal/design/PainelLogin.java
@@ -0,0 +1,41 @@
+package principal.design;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PainelLogin {
+    public static JPanel getPainelDeLogin(){
+        JPanel painelDeLogin = new JPanel(new GridLayout(3,1));
+        JPanel painelAuxiliar = new JPanel(new GridLayout(3, 3));
+        JPanel painelBotoes = new JPanel(new FlowLayout());
+        JLabel loginLabel = new JLabel("Login", JLabel.CENTER);
+        loginLabel.setFont(new Font(Font.SERIF, Font.BOLD, 20));
+        JLabel credencialLabel = new JLabel("Identificação:");
+        JLabel senhaLabel = new JLabel("Senha:");
+        JTextField credencialField = new JTextField();
+        JPasswordField senhaField = new JPasswordField();
+        JRadioButton pessoaRadioButton = new JRadioButton("Pessoa");
+        JRadioButton empresaRadioButton = new JRadioButton("Empresa");
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(pessoaRadioButton);
+        buttonGroup.add(empresaRadioButton);
+        Button entrarButton = new Button("Entrar");
+        Button cadastrarButton = new Button("cadastrar");
+
+        painelAuxiliar.add(credencialLabel);
+        painelAuxiliar.add(credencialField);
+        painelAuxiliar.add(senhaLabel);
+        painelAuxiliar.add(senhaField);
+        painelAuxiliar.add(pessoaRadioButton);
+        painelAuxiliar.add(empresaRadioButton);
+
+        painelBotoes.add(entrarButton);
+        painelBotoes.add(cadastrarButton);
+
+        painelDeLogin.add(loginLabel);
+        painelDeLogin.add(painelAuxiliar);
+        painelDeLogin.add(painelBotoes);
+
+        return painelDeLogin;
+    }
+}
