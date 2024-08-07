7257ee66fddd4fb5e9ace4379c3e1163be6c71cf
luis-fbs <luis-fbs@outlook.com>
2024-08-07 18:37:42 -0300

    Principal

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -2,8 +2,15 @@ package principal;
 
 import principal.design.Design;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Design design = new Design();
 
     }
