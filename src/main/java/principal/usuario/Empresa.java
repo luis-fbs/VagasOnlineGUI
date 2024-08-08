254a1d1a0e97e7ccbdbb1fcda3d4481d525b35c8
luis-fbs <luis-fbs@outlook.com>
2024-08-08 01:28:07 -0300

    Usuario empresa

---
+++ src/main/java/principal/usuario/Empresa.java
@@ -0,0 +1,13 @@
+package principal.usuario;
+
+public class Empresa {
+    String nome, foto;
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
