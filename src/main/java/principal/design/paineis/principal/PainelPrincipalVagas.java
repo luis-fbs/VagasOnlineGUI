2879cdb4f7856afa502b7f5e9747f421a03c7d31
luis-fbs <luis-fbs@outlook.com>
2024-08-08 12:12:09 -0300

    Muda abordagem na hora de montar painel de vagas

--- src/main/java/principal/design/paineis/principal/PainelPrincipalVagas.java
+++ src/main/java/principal/design/paineis/principal/PainelPrincipalVagas.java
@@ -1,5 +1,6 @@
 package principal.design.paineis.principal;
 
+import principal.design.paineis.vagas.PainelDeVagas;
 import principal.util.Conversor;
 import principal.util.ReferenciaURL;
 import principal.vaga.Vaga;
@@ -29,27 +30,8 @@ public class PainelPrincipalVagas {
 
             if (!response.body().isBlank()){
                 ArrayList<Vaga> vagas = Conversor.jsonParaListaDeVagas(response.body());
-                JPanel painelVagas = new JPanel(new GridLayout(vagas.size(), 1));
 
-                for (Vaga vaga : vagas){
-                    JPanel painelDaVaga = new JPanel(new GridLayout(3,1));
-                    JLabel empresa = new JLabel(vaga.getEmpresa(), JLabel.CENTER);
-                    empresa.setFont(new Font(Font.SERIF, Font.BOLD, 16));
-                    JLabel cargo = new JLabel(vaga.getCargo());
-                    JLabel descricao = new JLabel(vaga.getDescricao());
-                    descricao.setFont(new Font(Font.SERIF, Font.BOLD, 16));
-                    JScrollPane descricaoJScrollPane = new JScrollPane(descricao, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-
-                    painelDaVaga.add(empresa);
-                    painelDaVaga.add(cargo);
-                    painelDaVaga.add(descricaoJScrollPane);
-
-                    painelVagas.add(painelDaVaga);
-                }
-
-                JScrollPane vagasJScrollPane = new JScrollPane(painelVagas, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-
-                painelPrincipalVagas.add(vagasJScrollPane);
+                painelPrincipalVagas.add(PainelDeVagas.getPainelDeVagas(vagas));
 
             } else{
                 painelPrincipalVagas.add(new JLabel("Nenhuma vaga disponível"));
