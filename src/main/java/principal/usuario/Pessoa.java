8172b22311e1218268e90b7b7cefb89ebeaa54f8
luis-fbs <luis-fbs@outlook.com>
2024-08-08 01:27:48 -0300

    Usuario pessoa

---
+++ src/main/java/principal/usuario/Pessoa.java
@@ -0,0 +1,17 @@
+package principal.usuario;
+
+public class Pessoa {
+    String nome, curriculo, foto;
+
+    public String getCurriculo() {
+        return curriculo;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
