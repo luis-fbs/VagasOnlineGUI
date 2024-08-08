d3ba7199e7691d60e84ffdb3ac5f75342ba4ff17
luis-fbs <luis-fbs@outlook.com>
2024-08-08 11:12:29 -0300

    Cria getters

--- src/main/java/principal/vaga/Vaga.java
+++ src/main/java/principal/vaga/Vaga.java
@@ -2,4 +2,16 @@ package principal.vaga;
 
 public class Vaga {
     String cargo, descricao, empresa;
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
 }
